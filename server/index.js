115e0c771e40c2b73b8c7c18f9c0d7448009e99f
sunnyshrivastav <hackersunny89@gmail.com>
2026-04-01 00:22:35 +0530

    Update CORS configuration for allowed origins

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ app.post(
 );
 
 app.use(cors(
-    {origin:["http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "http://localhost:8000", "https://exam-notes-ai-powered-learning-asse.vercel.app", "https://exam-notes-ai-powered-learning-asse.vercel.app/auth"],
+    {origin:"https://examnotes-ai-client.onrender.com",
         credentials:true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
